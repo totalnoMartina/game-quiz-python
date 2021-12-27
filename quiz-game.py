43dafd55b4b916705557ae4721e28f08694a9c2d
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-27 19:18:47 +0000

    Add score increment, sum up score at the end

--- quiz-game.py
+++ quiz-game.py
@@ -6,28 +6,35 @@ if playing.lower() != 'yes':
     quit()
 
 print("Okay! Let's play!")
+score = 0
+
 
 answer = input("What berry has more benefits for our body frozen than fresh? ")
 if answer.lower() == "blueberry":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What citrus fruit has more calcium than cow milk? ")
 if answer.lower() == "orange":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What berry has more benefits for our body frozen than fresh? ")
 if answer.lower() == "blueberry":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What berry has more benefits for our body frozen than fresh? ")
 if answer.lower() == "blueberry":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
+print("You got " + str(score) + " questions correct!")
