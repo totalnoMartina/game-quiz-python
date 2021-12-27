03dee67d36c98c18e2733582f0427d05ae281abe
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-27 21:37:48 +0000

    Finish Guessing game practice

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -14,7 +14,29 @@ else:
     quit()
 
 r_number = random.randint(0, top_of_range)
-print(r_number)
+guesses = 0
 
 # includes also 11, needs start and stop arg
 # print(r_number)
+while True:
+    guesses += 1
+    user_guess = input("Take a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == r_number:
+        print("You got it!")
+        break
+    else:
+        if user_guess > r_number:
+            print("You were above the number!")
+        else:
+            print("You were below the number!")
+        
+
+
+print("You got it in", guesses, "guesses")
+
