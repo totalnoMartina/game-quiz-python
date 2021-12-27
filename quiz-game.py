6920dad09942d3a64e577dbab81e5f944d89612d
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-27 19:15:11 +0000

    Add four questions and lower method

--- quiz-game.py
+++ quiz-game.py
@@ -2,5 +2,32 @@ print("Welcome to Very Berry Quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != 'yes':
+if playing.lower() != 'yes':
     quit()
+
+print("Okay! Let's play!")
+
+answer = input("What berry has more benefits for our body frozen than fresh? ")
+if answer.lower() == "blueberry":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What citrus fruit has more calcium than cow milk? ")
+if answer.lower() == "orange":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What berry has more benefits for our body frozen than fresh? ")
+if answer.lower() == "blueberry":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What berry has more benefits for our body frozen than fresh? ")
+if answer.lower() == "blueberry":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
