f49cc4e38c86307fe85c44ac406f781231c1c20a
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-27 21:22:37 +0000

    Second game, guessing number randint usage

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -2,9 +2,19 @@ import random
 
 top_of_range = input("Type a number: ")
 
+
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
-r_number = random.randint(-5,11)
-# includes also 11
+    if top_of_range <= 0:
+        print("Please, type in number larger than zero next time")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+r_number = random.randint(0, top_of_range)
 print(r_number)
+
+# includes also 11, needs start and stop arg
+# print(r_number)
