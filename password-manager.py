f4cb343e042bbee7fb5d18645f468aa8df06b5a5
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-28 00:17:37 +0000

    Start last mini project using Python

---
+++ password-manager.py
@@ -0,0 +1,15 @@
+pwd = input("What is the master password?")
+
+def view():
+    pass
+
+def add():
+    pass
+
+mode = input("Would you like to add a new password, or use existing ones?(view / add) ").lower()
+if mode == "view":
+    print()
+elif mode == "add":
+    print()
+else:
+    print("Invalid mode")
